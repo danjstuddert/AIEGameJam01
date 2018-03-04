cd05a58362d6eb493e42077e4bb2eaf1968afdc2
Patrick Goodison <iPatt98@gmail.com>
2018-03-05 10:58:04 +1100

    Added player movement (with temporary keyboard movement)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody rb;
+    private Vector3 movementVector;
+    public float movementSpeed;
+    public GameObject truckPosition;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //temporary keyboard movement
+		if (Input.GetKeyDown(KeyCode.W))
+        {
+            movementVector.z = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            movementVector.z = -1;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            movementVector.x = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            movementVector.x = -1;
+        }
+
+        rb.AddForce(movementVector * movementSpeed);
+
+    }
+}
