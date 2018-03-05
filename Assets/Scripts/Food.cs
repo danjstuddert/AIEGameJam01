871a88577a2cf50ef4c09aaa8a8bd4be8ddb8acb
Patrick Goodison <iPatt98@gmail.com>
2018-03-06 10:24:12 +1100

    Food now has force applied

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -4,54 +4,40 @@ using UnityEngine;
 
 public class Food : MonoBehaviour {
 
-    public float effectiveRadius;
+    private float effectiveRadius;
     private Transform playerTruck;
-    private float timeSpentInAir;
     private Vector3 playerForward;
-    public float force;
+    public float horizontalForce;
+    public float verticalForce;
     public List<GameObject> ingredients;
 
 	// Use this for initialization
 	void Start () {
-        timeSpentInAir = 2.0f;
+        Vector3 newForce = playerForward * horizontalForce;
+        newForce.y += 3;
+        GetComponent<Rigidbody>().AddForce(newForce);
     }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        if (timeSpentInAir > 0)
-        {
-            Vector3 newForce = playerForward * force;
 
-
-            GetComponent<Rigidbody>().AddForce(newForce.normalized);
-            
-            timeSpentInAir -= Time.deltaTime;
-        }
 	}
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.GetComponent<Customers>())
-        {
-            //set customer target destination
-
-        }
-
         //get random ingredient
         int rand = Random.Range(0, ingredients.Count);
 
         Vector3 vec = transform.position;
-        vec.y = 0;
         Instantiate(ingredients[rand], vec, Quaternion.identity);
 
         Destroy(gameObject);
     }
 
-    public void Create(Transform truckPosition, float forceAppliedToFood, Vector3 forward)
+    public void Create(Transform truckPosition, Vector3 forward)
     {
         playerTruck = truckPosition;
         playerForward = forward;
-        force = forceAppliedToFood;
     }
 
 }
