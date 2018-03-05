e3a8b1f83fb82e5ac976ac0e3da77d4cfa4686db
Patrick Goodison <iPatt98@gmail.com>
2018-03-05 12:29:56 +1100

    Added score, attack and attract functions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,40 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XboxCtrlrInput;
 
 public class Player : MonoBehaviour {
 
     private Rigidbody rb;
     private Vector3 movementVector;
     public float movementSpeed;
-    public GameObject truckPosition;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject truckPosition;
+    private float score;
+    public float cooldown;
+    private float timeRemaining;
+
+    public XboxController controller;
+
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody>();
-	}
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        //temporary keyboard movement
-		if (Input.GetKeyDown(KeyCode.W))
+        movementVector.z = XCI.GetAxisRaw(XboxAxis.LeftStickY, controller);
+        movementVector.x = XCI.GetAxisRaw(XboxAxis.LeftStickX, controller);
+
+        rb.MovePosition(rb.position + movementVector * movementSpeed * Time.fixedDeltaTime);
+
+        if (XCI.GetAxis(XboxAxis.RightTrigger, controller) > 0.1f)
         {
-            movementVector.z = 1;
+            Attack();
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        
+        if (timeRemaining <= 0)
         {
-            movementVector.z = -1;
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            movementVector.x = 1;
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            movementVector.x = -1;
+            if (XCI.GetAxis(XboxAxis.LeftTrigger, controller) > 0.1f)
+            {
+                Attract();
+                timeRemaining = cooldown;
+            }
+            
         }
 
-        rb.AddForce(movementVector * movementSpeed);
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
 
+        }
+        
+    }
+
+    public void AddScore(float scoreToAdd)
+    {
+        score += scoreToAdd;
+    }
+
+    public float GetScore()
+    {
+        return score;
+    }
+
+    public void Attack()
+    {
+        
+    }
+
+    public void Attract()
+    {
+        
     }
 }
