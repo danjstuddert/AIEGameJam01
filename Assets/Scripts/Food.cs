75b42e01aaf33a284acaecfab6b98c1430964af8
Patrick Goodison <iPatt98@gmail.com>
2018-03-05 15:02:14 +1100

    Added rotation to player movement

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour {
+
+    public float launchSpeed;
+    public Transform playerTruck;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().AddExplosionForce(launchSpeed, transform.position, 5, 2);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Customers>())
+        {
+            //set target to shooter's truck
+        }
+
+        Destroy(gameObject);
+    }
+
+    public void Create(GameObject player)
+    {
+        
+    }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,12 +9,16 @@ public class Player : MonoBehaviour {
     private Vector3 movementVector;
     public float movementSpeed;
 
-    public GameObject truckPosition;
+    public Transform truckPosition;
     private float score;
     public float cooldown;
     private float timeRemaining;
 
     public XboxController controller;
+    public Quaternion rotation;
+
+    public GameObject temp;
+    public float forceAppliedToFood;
 
     // Use this for initialization
     void Start () {
@@ -23,32 +27,43 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        movementVector.z = XCI.GetAxisRaw(XboxAxis.LeftStickY, controller);
-        movementVector.x = XCI.GetAxisRaw(XboxAxis.LeftStickX, controller);
+        RotatePlayer();
 
+        movementVector.z = XCI.GetAxis(XboxAxis.LeftStickY, controller);
+        movementVector.x = XCI.GetAxis(XboxAxis.LeftStickX, controller);
+                
         rb.MovePosition(rb.position + movementVector * movementSpeed * Time.fixedDeltaTime);
-
-        if (XCI.GetAxis(XboxAxis.RightTrigger, controller) > 0.1f)
-        {
-            Attack();
-        }
-        
+                
         if (timeRemaining <= 0)
         {
-            if (XCI.GetAxis(XboxAxis.LeftTrigger, controller) > 0.1f)
+            if (XCI.GetAxis(XboxAxis.RightTrigger, controller) > 0.1f)
             {
-                Attract();
+                Attack();
                 timeRemaining = cooldown;
             }
-            
-        }
+        }       
 
         if (timeRemaining > 0)
         {
             timeRemaining -= Time.deltaTime;
+        }        
+    }
 
+    private void RotatePlayer()
+    {
+        float rotateAxisX = 0;
+        float rotateAxisZ = 0;
+
+        //get controller's right stick input
+        rotateAxisX = XCI.GetAxis(XboxAxis.RightStickX, controller);
+        rotateAxisZ = XCI.GetAxis(XboxAxis.RightStickY, controller);
+
+        Vector3 normaliseRotate = new Vector3(rotateAxisX, 0, rotateAxisZ);
+        //normaliseRotate = normaliseRotate.normalized;
+        if (normaliseRotate.normalized.x != 0 || normaliseRotate.normalized.z != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.Normalize(normaliseRotate));
         }
-        
     }
 
     public void AddScore(float scoreToAdd)
@@ -63,11 +78,8 @@ public class Player : MonoBehaviour {
 
     public void Attack()
     {
-        
-    }
-
-    public void Attract()
-    {
+        //instantiate object with rigidbody
+        Instantiate(temp, transform.position + transform.forward, Quaternion.identity);
         
     }
 }
