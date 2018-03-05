8c0ddc0dc27eee8702bd767d3d56a73dbc3be254
Matthew Le Nepveu <matthew@lenepveu.com>
2018-03-06 09:51:03 +1100

    NavMesh agents now go across the screen and get deleted once they get to the other side. Not sure if they seek a new distance when hit by food though as I can't test that yet.

--- Assets/Scripts/Customers.cs
+++ Assets/Scripts/Customers.cs
@@ -26,30 +26,30 @@ public class Customers : MonoBehaviour
 	// Update is called once per frame
 	void FixedUpdate()
     {
-        if ((customerHit) && (agent.pathStatus == NavMeshPathStatus.PathComplete) && (effect != null))
+        if (customerHit && agent.remainingDistance == 0 && (effect != null))
         {
             effect.Play();
         }
 
-        if ((!customerHit) && (agent.pathStatus == NavMeshPathStatus.PathComplete))
+        if (!customerHit && agent.remainingDistance == 0)
         {
-            DestroyObject(agent.gameObject);
+            Destroy(gameObject);
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == redFood)
+        if (collision.gameObject.CompareTag(redFood))
         {
             agent.destination = redTruck.transform.position;
+            customerHit = true;
         }
 
-        if (collision.gameObject.tag == greenFood)
+        if (collision.gameObject.CompareTag(greenFood))
         {
             agent.destination = greenTruck.transform.position;
+            customerHit = true;
         }
-
-        customerHit = true;
     }
 
     public void SetObjects(GameObject top, GameObject bottom, GameObject red, GameObject green)
