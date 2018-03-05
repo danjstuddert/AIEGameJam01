f7451291a0c4b755ffc511834cb9b474075a02a6
Matthew Le Nepveu <matthew@lenepveu.com>
2018-03-05 16:47:36 +1100

    Worked on the Customers and Spawn Customers script as well as refine the programming scene however I was unable to solve a bug to get the navmesh agents to be destroyed so I'll work on that tomorrow.

--- Assets/Scripts/Customers.cs
+++ Assets/Scripts/Customers.cs
@@ -5,18 +5,70 @@ using UnityEngine.AI;
 
 public class Customers : MonoBehaviour
 {
-    public Transform truck;
+    public ParticleSystem effect;
+    public string redFood;
+    public string greenFood;
 
-	// Use this for initialization
-	void Awake()
+    private NavMeshAgent agent;
+    private GameObject screenTop;
+    private GameObject screenBottom;
+    private GameObject redTruck;
+    private GameObject greenTruck;
+    private bool customerHit;
+
+    // Use this for initialization
+    void Awake()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = truck.position;
+        agent = GetComponent<NavMeshAgent>();
+        customerHit = false;
     }
 	
 	// Update is called once per frame
 	void FixedUpdate()
     {
-        
+        if ((customerHit) && (agent.pathStatus == NavMeshPathStatus.PathComplete) && (effect != null))
+        {
+            effect.Play();
+        }
+
+        if ((!customerHit) && (agent.pathStatus == NavMeshPathStatus.PathComplete))
+        {
+            DestroyObject(agent.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == redFood)
+        {
+            agent.destination = redTruck.transform.position;
+        }
+
+        if (collision.gameObject.tag == greenFood)
+        {
+            agent.destination = greenTruck.transform.position;
+        }
+
+        customerHit = true;
+    }
+
+    public void SetObjects(GameObject top, GameObject bottom, GameObject red, GameObject green)
+    {
+        screenTop = top;
+        screenBottom = bottom;
+        redTruck = red;
+        greenTruck = green;
+
+        SpawnCustomer spawnScript = FindObjectOfType<SpawnCustomer>();
+
+        if (spawnScript.GetPosition().z == spawnScript.topSpawnZ)
+        {
+            agent.destination = screenBottom.transform.position;
+        }
+
+        if (spawnScript.GetPosition().z == spawnScript.bottomSpawnZ)
+        {
+            agent.destination = screenTop.transform.position;
+        }
     }
 }
--- Assets/Scripts/SpawnCustomer.cs
+++ Assets/Scripts/SpawnCustomer.cs
@@ -5,27 +5,58 @@ using UnityEngine;
 public class SpawnCustomer : MonoBehaviour
 {
     public GameObject customer;
-    public Vector3 position;
-    public float customerSpawnTimer;
+    public GameObject screenTop;
+    public GameObject screenBottom;
+    public GameObject redTruck;
+    public GameObject greenTruck;
+    public float minSpawnX = -10.0f;
+    public float maxSpawnX = 10.0f;
+    public float spawnY = 0.0f;
+    public float bottomSpawnZ = -10.0f;
+    public float topSpawnZ = 10.0f;
+    public float spawnTimer = 2.0f;
 
     private List<GameObject> customerList;
+    private Vector3 position;
     private float timer;
 
     // Use this for initialization
     void Awake()
     {
         customerList = new List<GameObject>();
-        timer = 0.0f;
+        timer = spawnTimer;
     }
 	
 	// Update is called once per frame
 	void FixedUpdate()
     {
-        timer += Time.deltaTime;
+        timer -= Time.deltaTime;
 
-        if (timer % customerSpawnTimer == 0.0f)
+        if (timer <= 0.0f)
         {
-            customerList.Add(Instantiate(customer, position, Quaternion.identity));
+            int rand = Random.Range(0, 2);
+
+            if (rand == 0)
+            {
+                position = new Vector3(Random.Range(minSpawnX, maxSpawnX), 
+                                       spawnY, bottomSpawnZ);
+            }
+
+            if (rand == 1)
+            {
+                position = new Vector3(Random.Range(minSpawnX, maxSpawnX),
+                                       spawnY, topSpawnZ);
+            }
+
+            GameObject c = Instantiate(customer, position, Quaternion.identity);
+            customerList.Add(c);
+            c.GetComponent<Customers>().SetObjects(screenTop, screenBottom, redTruck, greenTruck);
+            timer = spawnTimer;
         }
+    }
+
+    public Vector3 GetPosition()
+    {
+        return position;
     }
 }
