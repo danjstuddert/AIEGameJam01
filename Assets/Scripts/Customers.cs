fd7a44e8a687b1240602af6edacff99a850d1da0
Matthew Le Nepveu <matthew@lenepveu.com>
2018-03-05 12:31:28 +1100

    Started work on the Customers script and the SpawnCustomer script, as well as add a scene for programmers to use.

---
+++ Assets/Scripts/Customers.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Customers : MonoBehaviour
+{
+    public Transform truck;
+
+	// Use this for initialization
+	void Awake()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = truck.position;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SpawnCustomer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCustomer : MonoBehaviour
+{
+    public GameObject customer;
+    public Vector3 position;
+    public float customerSpawnTimer;
+
+    private List<GameObject> customerList;
+    private float timer;
+
+    // Use this for initialization
+    void Awake()
+    {
+        customerList = new List<GameObject>();
+        timer = 0.0f;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+
+        if (timer % customerSpawnTimer == 0.0f)
+        {
+            customerList.Add(Instantiate(customer, position, Quaternion.identity));
+        }
+    }
+}
